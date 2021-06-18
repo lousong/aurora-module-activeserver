20731373c04a24ccebdcfc0dcb46840b2cdc2174
nadine-shi <nadinshi@gmail.com>
2021-06-18 16:36:49 +0300

    requiredModules functionality

--- vue/manager.js
+++ vue/manager.js
@@ -1,14 +1,18 @@
 import settings from '../../ActiveServer/vue/settings'
 
 export default {
-  name: 'ActiveServerWebclient',
+  moduleName: 'ActiveServer',
+
+  requiredModules: [],
+
   init (appData) {
     settings.init(appData)
   },
+
   getAdminSystemTabs () {
     return [
       {
-        name: 'activeserver-system',
+        tabName: 'activeserver-system',
         title: 'ACTIVESERVER.LABEL_SETTINGS_TAB',
         component () {
           return import('src/../../../ActiveServer/vue/components/ActiveSyncAdminSettings')
